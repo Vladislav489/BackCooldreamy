163ea1d5a2e0be7a55b0c0ae67044a9b9f994929
alexbrik8888 <itcyberdynesystems@gmail.com>
2023-11-18 22:09:15 +0300

    Update ChatMessageLogic.php

--- app/ModelAdmin/CoreEngine/LogicModels/Chat/ChatMessageLogic.php
+++ app/ModelAdmin/CoreEngine/LogicModels/Chat/ChatMessageLogic.php
@@ -96,7 +96,7 @@ class ChatMessageLogic extends CoreEngine {
         ],[DB::raw("MAX(".$this->engine->getTable().".id) as id"),
             DB::raw("ChatMessageSub.chat_messageable_type") ]);
         $lastMessage = $chatMessage->setJoin(
-            ['ChatMessageSub'])
+            ['ChatMessageSub','TextMessageSub'])
             ->setGroupBy(['chat_id'])->offPagination()->getGroup()['result'];
         return $lastMessage;
     }
