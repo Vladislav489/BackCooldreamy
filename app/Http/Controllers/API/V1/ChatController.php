b78d5fae920dd437fba3d5c61901b6532a4ccb6e
alexbrik8888 <itcyberdynesystems@gmail.com>
2023-11-24 17:07:28 +0300

    Update ChatController.php

--- app/Http/Controllers/API/V1/ChatController.php
+++ app/Http/Controllers/API/V1/ChatController.php
@@ -338,11 +338,7 @@ class ChatController extends Controller
             return response()->json(['error' => $validator->errors()], 500);
 
         $user_id = Auth::id();
-        $chat = (new ChatLogic())->getListChatUser($user_id,$request);
-
-
-        dd($chat);
-
+        $chat = (new ChatLogic())->getListChatUser($user_id,$request)[0];
         if(count($chat) == 0)
             return response()->json(['error' => 'You deleted the chat.'], 404);
 
