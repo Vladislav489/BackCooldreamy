bf8658fe02bf4c04caa67995973d295295c8aa39
martyn12 <87023187+martyn12@users.noreply.github.com>
2023-12-22 14:46:08 +0400

    cron delete real female users

--- app/Console/Commands/DeleteRealFemaleUsers.php
+++ app/Console/Commands/DeleteRealFemaleUsers.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Chat;
+use App\Models\User;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
@@ -27,8 +29,18 @@ class DeleteRealFemaleUsers extends Command
      */
     public function handle()
     {
-        $users = DB::table('users')->whereNotIn('id', [124518, 124519, 124487])->where([['gender', '=', 'female'], ['is_real', '=', 1]])->delete();
+        $users = DB::table('users')->where([['gender', '=', 'female'], ['is_real', '=', 1], ['is_operator', '=', 0]])->pluck('id')->toArray();
+        $chats = DB::table('chats')->whereIn('first_user_id', $users)->orWhereIn('second_user_id', $users)->pluck('id')->toArray();
+        $chat_messages = DB::table('chat_messages')->whereIn('chat_id', $chats)->delete();
+        $winks = DB::table('chat_wink_messages')->whereIn('from_user_id', $users)->orWhereIn('to_user_id', $users)->delete();
+        $images = DB::table('images')->whereIn('user_id', $users)->delete();
+        $users = User::whereIn('id', $users)->delete();
+        $chats = Chat::whereIn('id', $chats)->delete();
         $log = Log::build(['driver' => 'daily', 'path' => storage_path('logs/users/deleted_users.log')]);
         $log->info('Users deleted: ' . $users);
+        $log->info('Chats deleted: ' . $chats);
+        $log->info('Winks deleted: ' . $winks);
+        $log->info('Messages deleted: ' . $chat_messages);
+        $log->info('Images deleted: ' . $images);
     }
 }
