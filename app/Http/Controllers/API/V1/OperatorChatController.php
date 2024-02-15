d81c065a46c7fae44c1afc3ae4c6376329f71b84
martyn12 <87023187+martyn12@users.noreply.github.com>
2024-02-15 17:22:52 +0400

    Update OperatorChatController.php

--- app/Http/Controllers/API/V1/OperatorChatController.php
+++ app/Http/Controllers/API/V1/OperatorChatController.php
@@ -321,6 +321,7 @@ class OperatorChatController extends Controller
             return response()->json(['error' => $validator->errors()], 500);
         }
         $chat = $this->chatRepository->findForAnket($user, $id);
+        dd($chat);
         if (!OperatorLimitController::spendLimitsByOperator($chat->user_id, $chat->recepient_id, $chat->id)) {
             return response()->json(['error' => 'NO_LIMIT'], 500);
         }
