b0c39aea2c5ce9f35455f2177dd46fe288f2b2f1
martyn12 <87023187+martyn12@users.noreply.github.com>
2024-02-28 11:33:29 +0400

    Update ChatController.php

--- app/Http/Controllers/API/V1/ChatController.php
+++ app/Http/Controllers/API/V1/ChatController.php
@@ -1007,7 +1007,7 @@ class ChatController extends Controller
         $chat_messages = $chat->chat_messages()
             ->with(['sender_user' => function ($query) {
                 $query->select('id', 'name', 'avatar_url_thumbnail');
-            }, 'chat_messageable'])
+            }, 'chat_messageable', 'chat_messageable.gifts', 'chat_messageable.sticker'])
             ->orderBy('created_at', 'desc')
             ->paginate($perPage);
         return $chat_messages;
