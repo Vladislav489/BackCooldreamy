fe47357d2731a27138d1f267ddc2bea3f3cd53bd
martyn12 <87023187+martyn12@users.noreply.github.com>
2024-02-28 13:16:54 +0400

    Update OperatorMessageController.php

--- app/Http/Controllers/API/V1/OperatorMessageController.php
+++ app/Http/Controllers/API/V1/OperatorMessageController.php
@@ -147,7 +147,7 @@ class OperatorMessageController extends Controller
         $operatorPayments = new OperatorCreditsLogic($params, [DB::raw('SUM(credits) as credits, message_type')]);
         $operatorPayments->offPagination()->getFullQuery()->groupBy('message_type');
         $result = $operatorPayments->getList();
-        foreach ($result as $item) {
+        foreach ($result['result'] as $item) {
             if (is_null($item['message_type'])) {
                 unset($item);
             }
