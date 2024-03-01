1b5ef46d725af60639d0bc91bb916efcced9fc63
martyn12 <87023187+martyn12@users.noreply.github.com>
2024-03-01 19:33:29 +0400

    Update CreditsController.php

--- app/Http/Controllers/API/V1/CreditsController.php
+++ app/Http/Controllers/API/V1/CreditsController.php
@@ -103,6 +103,7 @@ class CreditsController extends Controller
 
             //оплата только лоя мужчин
             $result = $user->check_payment_man($servicePrice->price,$service_id,$action,$second_user_id);
+            dd($result);
             // если не false знасит все прошло успешно
             if ($result) {
                 return $result;
